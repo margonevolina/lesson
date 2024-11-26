65eaa33e09a0476331734a50f2df5145e2e00ffd
margonevolina <margo6984@gmail.com>
2024-11-26 13:41:30 +0300

    Add files via upload

---
+++ Practical_task_module_1.py
@@ -0,0 +1,10 @@
+grades=[[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+grades=sum(grades[0])/len(grades[0]),sum(grades[1])/len(grades[1]),sum(grades[2])/len(grades[2]),sum(grades[3])/len(grades[3]),sum(grades[4])/len(grades[4])
+print(grades)
+students={"Johnny","Bilbo","Steve","Khendrik","Aaron"}
+students=list({"Johnny","Bilbo","Steve","Khendrik","Aaron"})
+print(students)
+students=sorted(students)
+print(students)
+my_dict=dict(zip(students,grades))
+print(my_dict)
